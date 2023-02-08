2b8748f44f6db3abac434d7205a5fdb30bc24922
Dan Maffia <73266204+dan998@users.noreply.github.com>
2023-02-08 00:19:42 -0400

    Add files via upload

---
+++ update/telegramchannel.sh
@@ -0,0 +1,3 @@
+am start -a android.intent.action.VIEW -d https://t.me/danmaffiayounghack 2>/dev/null
+clear
+menu
---
+++ update/youtubechannel.sh
@@ -0,0 +1,3 @@
+am start -a android.intent.action.VIEW -d http://YouTube.com/@techindn 2>/dev/null
+clear
+menu
